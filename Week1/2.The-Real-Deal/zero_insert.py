f9a09a0b3033e21f99e7202006791ae30eed6a42
pgenev <philip_genev@abv.bg>
2017-10-31 14:09:43 +0200

    task done

--- Week1/2.The-Real-Deal/zero_insert.py
+++ Week1/2.The-Real-Deal/zero_insert.py
@@ -1,16 +1,17 @@
 def zero_insert(number):
     number_list = [int(digit) for digit in str(number)]
-    for index in range(1, len(number_list)):
-        if number_list[index-1] == number_list[index]:
-            number_list.insert(index, 0)
-        elif (number_list[index-1] + number_list[index]) % 10 == 0:
-            number_list.insert(index, 0)
+    item_index = 1
+    while(True):
+        if item_index >= len(number_list):
+            break
+        if number_list[item_index - 1] == number_list[item_index]:
+            number_list.insert(item_index, 0)
+        elif (number_list[item_index-1] + number_list[item_index]) % 10 == 0:
+            number_list.insert(item_index, 0)
+        item_index += 1
     return "".join([str(number) for number in number_list])
-
-
 
 print(zero_insert(116457))
 print(zero_insert(55555555))
-zero_insert(55555555)
-# print(zero_insert(1))
-# print(zero_insert(6446))
+print(zero_insert(1))
+print(zero_insert(6446))
