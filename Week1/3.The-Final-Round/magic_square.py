c78978f6ae8bc03b780bb3c9d79231dadf4edf85
pgenev <philip_genev@abv.bg>
2017-11-06 17:04:05 +0200

    in progress

--- Week1/3.The-Final-Round/magic_square.py
+++ Week1/3.The-Final-Round/magic_square.py
@@ -1,13 +1,15 @@
-def magic_square(matrix):
+#refactoring needed
 
-    return antidiagonal_sum(matrix)
+def magic_square(matrix):
+    return rows_sum(matrix) == cols_sum(matrix) == main_diagonal_sum(matrix) == antidiagonal_sum(matrix)
+
 
 def rows_sum(matrix):
     single_row_sum = sum(matrix[0])
     for row in matrix:
         if single_row_sum != sum(row):
             return False
-    return True
+    return single_row_sum
 
 def cols_sum(matrix):
     transposed_matrix = []
@@ -21,12 +23,20 @@ def cols_sum(matrix):
     for row in transposed_matrix:
         if single_col_sum != sum(row):
             return False
-    return True
+    return single_col_sum
 
 def main_diagonal_sum(matrix):
     return sum([matrix[index][index] for index in range(0, len(matrix))])
 
 def antidiagonal_sum(matrix):
+    row = 0
+    col = len(matrix) - 1
+    sum = 0
+    while(col != -1):
+        sum += matrix[row][col]
+        row += 1
+        col -= 1
+    return sum
 
 
 
@@ -34,8 +44,9 @@ def antidiagonal_sum(matrix):
 
 
 
-# print(magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+print(magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 print(magic_square([[4, 9, 2], [3, 5, 7], [8, 1, 6]]))
-# print(magic_square([[7, 12, 1, 14], [2, 13, 8, 11], [16, 3, 10, 5], [9, 6, 15, 4]]))
-# print(magic_square([[23, 28, 21], [22, 24, 26], [27, 20, 25]]))
-# print(magic_square([[16, 23, 17], [78, 32, 21], [17, 16, 15]]))
+print(magic_square([[7, 12, 1, 14], [2, 13, 8, 11], [16, 3, 10, 5], [9, 6, 15, 4]]))
+print(magic_square([[23, 28, 21], [22, 24, 26], [27, 20, 25]]))
+print(magic_square([[16, 23, 17], [78, 32, 21], [17, 16, 15]]))
